818c72964bc3f95eb6a624c1b97d8d833df6bdb8
Christian Ramelow <info@codekandis.net>
2020-05-06 17:15:29 +0200

    Adds the class `AbstractNullableFloat`.

---
+++ src/Types/Wrapped/AbstractNullableFloat.php
@@ -0,0 +1,51 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Shorty\Types\Wrapped;
+
+use CodeKandis\Shorty\AbstractObject;
+
+/**
+ * Represents the base class of all wrapped `nullable float` types.
+ * @package codekandis/shorty
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+abstract class AbstractNullableFloat extends AbstractObject implements NullableFloatInterface
+{
+	/**
+	 * Stores the value of the wrapped `nullable float`.
+	 * @var null|float
+	 */
+	protected ?float $value;
+
+	/**
+	 * Constructor method.
+	 * @param null|float $value The `nullable float` value to wrap.
+	 */
+	public function __construct( ?float $value = null )
+	{
+		$this->value = $value;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public static function fromNullableFloat( NullableFloatInterface $value ): NullableFloatInterface
+	{
+		return new static( $value->getValue() );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getValue(): ?float
+	{
+		return $this->value;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function toString(): string
+	{
+		return null === $this->value ? 'null' : (string) $this->value;
+	}
+}
