a23c199f95a03b9effd17e185eea99a8114595ef
Christian Ramelow <info@codekandis.net>
2020-05-06 17:15:08 +0200

    Adds the class `AbstractNullableInt`.

---
+++ src/Types/Wrapped/AbstractNullableInt.php
@@ -0,0 +1,51 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Shorty\Types\Wrapped;
+
+use CodeKandis\Shorty\AbstractObject;
+
+/**
+ * Represents the base class of all wrapped `nullable int` types.
+ * @package codekandis/shorty
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+abstract class AbstractNullableInt extends AbstractObject implements NullableIntInterface
+{
+	/**
+	 * Stores the value of the wrapped `nullable int`.
+	 * @var null|int
+	 */
+	protected ?int $value;
+
+	/**
+	 * Constructor method.
+	 * @param null|int $value The `nullable int` value to wrap.
+	 */
+	public function __construct( ?int $value = null )
+	{
+		$this->value = $value;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public static function fromNullableInt( NullableIntInterface $value ): NullableIntInterface
+	{
+		return new static( $value->getValue() );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getValue(): ?int
+	{
+		return $this->value;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function toString(): string
+	{
+		return null === $this->value ? 'null' : (string) $this->value;
+	}
+}
