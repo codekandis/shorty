a0bc45a4620c9013b27a223215b37fe263c260e1
Christian Ramelow <info@codekandis.net>
2020-05-06 16:51:55 +0200

    Adds the exception `IndexIsInvalidException`.

---
+++ src/IndexIsInvalidException.php
@@ -0,0 +1,11 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Shorty;
+
+/**
+ * Represents an exception if an index is invalid.
+ * @package codekandis/shorty
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class IndexIsInvalidException extends ValueIsInvalidException
+{
+}
