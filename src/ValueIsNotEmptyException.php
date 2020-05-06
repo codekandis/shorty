3eab6bd807c1a013262e1c5e07f0e5cbbb605c36
Christian Ramelow <info@codekandis.net>
2020-05-06 16:39:09 +0200

    Adds the exception `ValueIsNotEmptyException`.

---
+++ src/ValueIsNotEmptyException.php
@@ -0,0 +1,11 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Shorty;
+
+/**
+ * Represents an exception if a value is not empty.
+ * @package codekandis/shorty
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class ValueIsNotEmptyException extends ValueIsInvalidException
+{
+}
