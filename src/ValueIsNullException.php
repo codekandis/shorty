0de357cf395ba23c032e0b52c3f1628ca9632cd0
Christian Ramelow <info@codekandis.net>
2020-05-06 16:32:58 +0200

    Adds the exception `ValueIsNullException`.

---
+++ src/ValueIsNullException.php
@@ -0,0 +1,11 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Shorty;
+
+/**
+ * Represents an exception if a value is null.
+ * @package codekandis/shorty
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class ValueIsNullException extends ValueIsInvalidException
+{
+}
