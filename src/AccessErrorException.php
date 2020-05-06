2f5ae51850168ddab9ffab964f41c8d7870d2b5d
Christian Ramelow <info@codekandis.net>
2020-05-06 16:11:43 +0200

    Adds the exception `AccessErrorException`.

---
+++ src/AccessErrorException.php
@@ -0,0 +1,11 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Shorty;
+
+/**
+ * Represents an exception if an access error occured.
+ * @package codekandis/shorty
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class AccessErrorException extends LogicException
+{
+}
