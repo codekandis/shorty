487f2ae936d9286b3fed2cc48d977ee5d821ed9d
Christian Ramelow <info@codekandis.net>
2020-05-06 16:14:04 +0200

    Adds the exception `ValueIsInvalidException`.

---
+++ src/ValueIsInvalidException.php
@@ -0,0 +1,11 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Shorty;
+
+/**
+ * Represents an exception if a value is invalid.
+ * @package codekandis/shorty
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class ValueIsInvalidException extends LogicException
+{
+}
