71aac1222e2dd6ea7a74f07b56f8f801c3787328
Christian Ramelow <info@codekandis.net>
2020-05-06 15:49:55 +0200

    Adds the exception `LogicException`.

---
+++ src/LogicException.php
@@ -0,0 +1,13 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Shorty;
+
+use LogicException as LogicException_Origin;
+
+/**
+ * Represents the base exception of all logic exceptions.
+ * @package codekandis/shorty
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class LogicException extends LogicException_Origin
+{
+}
