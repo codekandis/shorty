7510af823118a749439ad0035e6d29cd3c4cc838
Christian Ramelow <info@codekandis.net>
2020-05-06 15:42:34 +0200

    Adds the interface `ObjectInterface`.

---
+++ src/ObjectInterface.php
@@ -0,0 +1,16 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Shorty;
+
+/**
+ * Represents the interface of all objects.
+ * @package codekandis/shorty
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+interface ObjectInterface
+{
+	/**
+	 * Returns the `string` representation of the object.
+	 * @return string The `string` representation of the object.
+	 */
+	public function __toString(): string;
+}
