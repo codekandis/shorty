074144e0e3cc0a67acb9b934063f2af30804dd9e
Christian Ramelow <info@codekandis.net>
2020-05-06 16:17:09 +0200

    Adds the exception `ErrorException`.

---
+++ src/ErrorException.php
@@ -0,0 +1,11 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Shorty;
+
+/**
+ * Represents an exception if a general error occured.
+ * @package codekandis/shorty
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class ErrorException extends RuntimeException
+{
+}
