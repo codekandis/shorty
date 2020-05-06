98891dbfad17cd9bb4b59bbc626fbaafb4b0643a
Christian Ramelow <info@codekandis.net>
2020-05-06 17:14:30 +0200

    Adds the class `AbstractNullableString`.

---
+++ src/Types/Wrapped/AbstractNullableString.php
@@ -0,0 +1,52 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Shorty\Types\Wrapped;
+
+use CodeKandis\Shorty\AbstractObject;
+
+/**
+ * Represents the base class of all wrapped `nullable string` types.
+ * @package codekandis/shorty
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+abstract class AbstractNullableString extends AbstractObject implements NullableStringInterface
+{
+	/**
+	 * Stores the value of the wrapped `nullable string`.
+	 * @var null|string
+	 */
+	protected ?string $value;
+
+	/**
+	 * Constructor method.
+	 * @param null|string $value The `nullable string` value to wrap.
+	 */
+	public function __construct( ?string $value = null )
+	{
+		$this->value = $value;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public static function fromNullableString( NullableStringInterface $value ): NullableStringInterface
+	{
+		return new static( $value->getValue() );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getValue(): ?string
+	{
+		return $this->value;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function toString(): string
+	{
+		return $this->value
+		       ?? 'null';
+	}
+}
