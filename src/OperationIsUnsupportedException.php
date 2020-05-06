fa4a434616d8d9e8ef36bc8d0dc5191452e95980
Christian Ramelow <info@codekandis.net>
2020-05-06 16:26:27 +0200

    Adds the exception `OperationIsUnsupportedException`.

---
+++ src/OperationIsUnsupportedException.php
@@ -0,0 +1,11 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Shorty;
+
+/**
+ * Represents an exception if an operation is unsupported.
+ * @package codekandis/shorty
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class OperationIsUnsupportedException extends AccessErrorException
+{
+}
