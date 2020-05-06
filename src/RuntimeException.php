31f23a6747645835d0891d965875daf10eda1baf
Christian Ramelow <info@codekandis.net>
2020-05-06 15:49:36 +0200

    Adds the exception `RuntimeException`.

---
+++ src/RuntimeException.php
@@ -0,0 +1,13 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Shorty;
+
+use RuntimeException as RuntimeException_Origin;
+
+/**
+ * Represents the base exception of all runtime exceptions.
+ * @package codekandis/shorty
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class RuntimeException extends RuntimeException_Origin
+{
+}
