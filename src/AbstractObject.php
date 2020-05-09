651e0f272956788479257eacb1416a62eb816446
Christian Ramelow <info@codekandis.net>
2020-05-09 22:55:15 +0200

    Adds the method `AbstractObject::getType()`.

--- src/AbstractObject.php
+++ src/AbstractObject.php
@@ -12,10 +12,25 @@ use function get_called_class;
 abstract class AbstractObject extends stdClass implements ObjectInterface
 {
 	/**
+	 * Stores the type of the object.
+	 * @var null|TypeInterface
+	 */
+	private ?TypeInterface $type;
+
+	/**
 	 * @inheritDoc
 	 */
 	public function __toString(): string
 	{
 		return get_called_class();
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getType(): TypeInterface
+	{
+		return $this->type
+		       ?? $this->type = new Type( $this );
 	}
 }
