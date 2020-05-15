81b79fa778c09c68a90bc6b0053678ff005a7305
Christian Ramelow <info@codekandis.net>
2020-05-16 00:48:37 +0200

    Adds the exception `DeserializationFailedException`.

---
+++ src/Data/Serialization/DeserializationFailedException.php
@@ -0,0 +1,11 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Shorty\Data\Serialization;
+
+/**
+ * Represents an exception if a deserialization failed.
+ * @package codekandis/shorty
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class DeserializationFailedException extends SerializerException
+{
+}
