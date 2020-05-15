d99096d2470f77f77936b19b30d8269330dfa749
Christian Ramelow <info@codekandis.net>
2020-05-15 11:26:28 +0200

    Adds the exception `IndexIsOutOfBoundsException`.

---
+++ src/Collections/IndexIsOutOfBoundsException.php
@@ -0,0 +1,13 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Shorty\Collections;
+
+use CodeKandis\Shorty\IndexIsInvalidException;
+
+/**
+ * Represents an exception if an index is out of bounds.
+ * @package codekandis/shorty
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class IndexIsOutOfBoundsException extends IndexIsInvalidException
+{
+}
