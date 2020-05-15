0e20fbb364dc1292c3b245936176d7d4ba886bd3
Christian Ramelow <info@codekandis.net>
2020-05-16 00:47:48 +0200

    Adds the exception `SerializationFailedException`.

---
+++ src/Data/Serialization/SerializationFailedException.php
@@ -0,0 +1,11 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Shorty\Data\Serialization;
+
+/**
+ * Represents an exception if a serialization failed.
+ * @package codekandis/shorty
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class SerializationFailedException extends SerializerException
+{
+}
