3212810b9bccf11ee9bfb0aaedc51a98b1ecc6c4
Christian Ramelow <info@codekandis.net>
2020-05-16 00:45:40 +0200

    Adds the exception `SerializationContractNotFoundException`.

---
+++ src/Data/Serialization/SerializationContractNotFoundException.php
@@ -0,0 +1,11 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Shorty\Data\Serialization;
+
+/**
+ * Represents an exception if a serialization contract does not exist.
+ * @package codekandis/shorty
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class SerializationContractNotFoundException extends SerializerException
+{
+}
