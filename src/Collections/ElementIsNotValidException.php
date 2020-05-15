990131c020d995da9ae1a8a12fda95431b424381
Christian Ramelow <info@codekandis.net>
2020-05-15 11:26:08 +0200

    Adds the exception `ElementIsNotValidException`.

---
+++ src/Collections/ElementIsNotValidException.php
@@ -0,0 +1,13 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Shorty\Collections;
+
+use CodeKandis\Shorty\ValueIsInvalidException;
+
+/**
+ * Represents an exception if an element does not exist.
+ * @package codekandis/shorty
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class ElementIsNotValidException extends ValueIsInvalidException
+{
+}
