9dc801d1d58bd81cba243e639775269e26eb1b0a
Christian Ramelow <info@codekandis.net>
2020-05-16 09:43:17 +0200

    Makes the class `AbstractBool` serializable.

--- src/Types/Wrapped/AbstractBool.php
+++ src/Types/Wrapped/AbstractBool.php
@@ -2,17 +2,21 @@
 namespace CodeKandis\Shorty\Types\Wrapped;
 
 use CodeKandis\Shorty\AbstractObject;
+use CodeKandis\Shorty\Data\Serialization\SerializationContractAttribute;
+use CodeKandis\Shorty\Data\Serialization\SerializationPropertyAttribute;
 
 /**
  * Represents the base class of all wrapped `bool` types.
  * @package codekandis/shorty
  * @author Christian Ramelow <info@codekandis.net>
+ * @SerializationContractAttribute()
  */
 abstract class AbstractBool extends AbstractObject implements BoolInterface
 {
 	/**
 	 * Stores the value of the wrapped `bool`.
 	 * @var bool
+	 * @SerializationPropertyAttribute()
 	 */
 	protected bool $value;
 
