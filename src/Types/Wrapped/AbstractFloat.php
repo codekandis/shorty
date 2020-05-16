b778bbf6466ede5f56a2ace7312d90f347124381
Christian Ramelow <info@codekandis.net>
2020-05-16 09:42:52 +0200

    Makes the class `AbstractFloat` serializable.

--- src/Types/Wrapped/AbstractFloat.php
+++ src/Types/Wrapped/AbstractFloat.php
@@ -2,17 +2,21 @@
 namespace CodeKandis\Shorty\Types\Wrapped;
 
 use CodeKandis\Shorty\AbstractObject;
+use CodeKandis\Shorty\Data\Serialization\SerializationContractAttribute;
+use CodeKandis\Shorty\Data\Serialization\SerializationPropertyAttribute;
 
 /**
  * Represents the base class of all wrapped `float` types.
  * @package codekandis/shorty
  * @author Christian Ramelow <info@codekandis.net>
+ * @SerializationContractAttribute()
  */
 abstract class AbstractFloat extends AbstractObject implements FloatInterface
 {
 	/**
 	 * Stores the value of the wrapped `float`.
 	 * @var float
+	 * @SerializationPropertyAttribute()
 	 */
 	protected float $value;
 
