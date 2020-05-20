8c8193d5aca351234d6624acbefb7bd8562e5de8
Christian Ramelow <info@codekandis.net>
2020-05-20 22:34:48 +0200

    Makes the class `AbstractReferencedInt` serializable.

--- src/Types/Wrapped/AbstractReferencedInt.php
+++ src/Types/Wrapped/AbstractReferencedInt.php
@@ -2,17 +2,21 @@
 namespace CodeKandis\Shorty\Types\Wrapped;
 
 use CodeKandis\Shorty\AbstractObject;
+use CodeKandis\Shorty\Data\Serialization\SerializationContractAttribute;
+use CodeKandis\Shorty\Data\Serialization\SerializationPropertyAttribute;
 
 /**
  * Represents the base class of all wrapped referenced `int` types.
  * @package codekandis/shorty
  * @author Christian Ramelow <info@codekandis.net>
+ * @SerializationContractAttribute( serializeSinglePropertyOnly = true )
  */
 abstract class AbstractReferencedInt extends AbstractObject implements ReferencedIntInterface
 {
 	/**
 	 * Stores the value of the wrapped referenced `int`.
 	 * @var int
+	 * @SerializationPropertyAttribute()
 	 */
 	protected int $value;
 
