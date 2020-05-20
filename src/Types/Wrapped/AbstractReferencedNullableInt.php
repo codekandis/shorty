f66afb08ebf52eb5dfcf92a58657c6d5fe5c8c97
Christian Ramelow <info@codekandis.net>
2020-05-20 22:35:32 +0200

    Makes the class `AbstractReferencedNullableInt` serializable.

--- src/Types/Wrapped/AbstractReferencedNullableInt.php
+++ src/Types/Wrapped/AbstractReferencedNullableInt.php
@@ -2,17 +2,21 @@
 namespace CodeKandis\Shorty\Types\Wrapped;
 
 use CodeKandis\Shorty\AbstractObject;
+use CodeKandis\Shorty\Data\Serialization\SerializationContractAttribute;
+use CodeKandis\Shorty\Data\Serialization\SerializationPropertyAttribute;
 
 /**
  * Represents the base class of all wrapped referenced `nullable int` types.
  * @package codekandis/shorty
  * @author Christian Ramelow <info@codekandis.net>
+ * @SerializationContractAttribute( serializeSinglePropertyOnly = true )
  */
 abstract class AbstractReferencedNullableInt extends AbstractObject implements ReferencedNullableIntInterface
 {
 	/**
 	 * Stores the value of the wrapped referenced `nullable int`.
 	 * @var null|int
+	 * @SerializationPropertyAttribute()
 	 */
 	protected ?int $value;
 
