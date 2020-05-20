eafb8e95ce3202cf300c1fc062485f2e7bd39a9c
Christian Ramelow <info@codekandis.net>
2020-05-20 21:06:17 +0200

    Adds the class `AbstractReferencedFloat`.

---
+++ src/Types/Wrapped/AbstractReferencedFloat.php
@@ -0,0 +1,51 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Shorty\Types\Wrapped;
+
+use CodeKandis\Shorty\AbstractObject;
+
+/**
+ * Represents the base class of all wrapped referenced `float` types.
+ * @package codekandis/shorty
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+abstract class AbstractReferencedFloat extends AbstractObject implements ReferencedFloatInterface
+{
+	/**
+	 * Stores the value of the wrapped referenced `float`.
+	 * @var float
+	 */
+	protected float $value;
+
+	/**
+	 * Constructor method.
+	 * @param float $value The `float` value to wrap.
+	 */
+	public function __construct( float &$value = 0 )
+	{
+		$this->value = &$value;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public static function fromReferencedFloat( ReferencedFloatInterface $value ): ReferencedFloatInterface
+	{
+		return new static( $value->getValue() );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function &getValue(): float
+	{
+		return $this->value;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function toString(): string
+	{
+		return (string) $this->value;
+	}
+}
