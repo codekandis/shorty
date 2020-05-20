d62c7ddadd7b61232f53bd1b6dd3b6f14a7da2cf
Christian Ramelow <info@codekandis.net>
2020-05-20 22:33:32 +0200

    Makes the class `AbstractReferencedString` serializable.

--- src/Types/Wrapped/AbstractReferencedString.php
+++ src/Types/Wrapped/AbstractReferencedString.php
@@ -2,17 +2,21 @@
 namespace CodeKandis\Shorty\Types\Wrapped;
 
 use CodeKandis\Shorty\AbstractObject;
+use CodeKandis\Shorty\Data\Serialization\SerializationContractAttribute;
+use CodeKandis\Shorty\Data\Serialization\SerializationPropertyAttribute;
 
 /**
  * Represents the base class of all wrapped referenced `string` types.
  * @package codekandis/shorty
  * @author Christian Ramelow <info@codekandis.net>
+ * @SerializationContractAttribute( serializeSinglePropertyOnly = true )
  */
 abstract class AbstractReferencedString extends AbstractObject implements ReferencedStringInterface
 {
 	/**
 	 * Stores the value of the wrapped referenced `string`.
 	 * @var string
+	 * @SerializationPropertyAttribute()
 	 */
 	protected string $value;
 
