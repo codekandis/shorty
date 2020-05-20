b485f5cf0650d4732fb8f77eca0719dba827ebb1
Christian Ramelow <info@codekandis.net>
2020-05-20 22:34:16 +0200

    Makes the class `AbstractReferencedNullableString` serializable.

--- src/Types/Wrapped/AbstractReferencedNullableString.php
+++ src/Types/Wrapped/AbstractReferencedNullableString.php
@@ -2,17 +2,21 @@
 namespace CodeKandis\Shorty\Types\Wrapped;
 
 use CodeKandis\Shorty\AbstractObject;
+use CodeKandis\Shorty\Data\Serialization\SerializationContractAttribute;
+use CodeKandis\Shorty\Data\Serialization\SerializationPropertyAttribute;
 
 /**
  * Represents the base class of all wrapped referenced `nullable string` types.
  * @package codekandis/shorty
  * @author Christian Ramelow <info@codekandis.net>
+ * @SerializationContractAttribute( serializeSinglePropertyOnly = true )
  */
 abstract class AbstractReferencedNullableString extends AbstractObject implements ReferencedNullableStringInterface
 {
 	/**
 	 * Stores the value of the wrapped referenced `nullable string`.
 	 * @var null|string
+	 * @SerializationPropertyAttribute()
 	 */
 	protected ?string $value;
 
