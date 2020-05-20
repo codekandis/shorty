1877688f7aabb155460f12bbdb167da5b3bda4e8
Christian Ramelow <info@codekandis.net>
2020-05-20 22:38:03 +0200

    Makes the class `AbstractReferencedBool` serializable.

--- src/Types/Wrapped/AbstractReferencedBool.php
+++ src/Types/Wrapped/AbstractReferencedBool.php
@@ -2,17 +2,21 @@
 namespace CodeKandis\Shorty\Types\Wrapped;
 
 use CodeKandis\Shorty\AbstractObject;
+use CodeKandis\Shorty\Data\Serialization\SerializationContractAttribute;
+use CodeKandis\Shorty\Data\Serialization\SerializationPropertyAttribute;
 
 /**
  * Represents the base class of all wrapped referenced `bool` types.
  * @package codekandis/shorty
  * @author Christian Ramelow <info@codekandis.net>
+ * @SerializationContractAttribute( serializeSinglePropertyOnly = true )
  */
 abstract class AbstractReferencedBool extends AbstractObject implements ReferencedBoolInterface
 {
 	/**
 	 * Stores the value of the wrapped referenced `bool`.
 	 * @var bool
+	 * @SerializationPropertyAttribute()
 	 */
 	protected bool $value;
 
