fa49524145bad70784b103aa47ff4dd66cd404a9
Christian Ramelow <info@codekandis.net>
2020-05-20 22:37:30 +0200

    Makes the class `AbstractReferencedNullableFloat` serializable.

--- src/Types/Wrapped/AbstractReferencedNullableFloat.php
+++ src/Types/Wrapped/AbstractReferencedNullableFloat.php
@@ -2,17 +2,21 @@
 namespace CodeKandis\Shorty\Types\Wrapped;
 
 use CodeKandis\Shorty\AbstractObject;
+use CodeKandis\Shorty\Data\Serialization\SerializationContractAttribute;
+use CodeKandis\Shorty\Data\Serialization\SerializationPropertyAttribute;
 
 /**
  * Represents the base class of all wrapped referenced `nullable float` types.
  * @package codekandis/shorty
  * @author Christian Ramelow <info@codekandis.net>
+ * @SerializationContractAttribute( serializeSinglePropertyOnly = true )
  */
 abstract class AbstractReferencedNullableFloat extends AbstractObject implements ReferencedNullableFloatInterface
 {
 	/**
 	 * Stores the value of the wrapped referenced `nullable float`.
 	 * @var null|float
+	 * @SerializationPropertyAttribute()
 	 */
 	protected ?float $value;
 
