b1a28a70350af0f07065dfb965f954cdf5d3a765
Christian Ramelow <info@codekandis.net>
2020-05-22 17:33:28 +0200

    Adds the exception `FileNotDeletableException`.

---
+++ src/Io/FileIsNotDeletableException.php
@@ -0,0 +1,11 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Shorty\Io;
+
+/**
+ * Represents the exception if a file is not deletable.
+ * @package codekandis/shorty
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class FileIsNotDeletableException extends FileSystemEntryIsNotDeletableException
+{
+}
