715b2d5214803e8c0a4cd1e9192524d368ab3e7f
Christian Ramelow <info@codekandis.net>
2020-05-22 14:48:53 +0200

    Makes the class `AbstractArray` serializable.

--- src/Types/Wrapped/AbstractArray.php
+++ src/Types/Wrapped/AbstractArray.php
@@ -2,6 +2,8 @@
 namespace CodeKandis\Shorty\Types\Wrapped;
 
 use CodeKandis\Shorty\AbstractObject;
+use CodeKandis\Shorty\Data\Serialization\SerializationContractAttribute;
+use CodeKandis\Shorty\Data\Serialization\SerializationPropertyAttribute;
 use function array_filter;
 use function implode;
 use function preg_replace;
@@ -12,12 +14,14 @@ use function var_export;
  * Represents the base class of all wrapped `array` types.
  * @package codekandis/shorty
  * @author Christian Ramelow <info@codekandis.net>
+ * @SerializationContractAttribute( serializeSinglePropertyOnly = true )
  */
 abstract class AbstractArray extends AbstractObject implements ArrayInterface
 {
 	/**
 	 * Stores the value of the wrapped `array`.
 	 * @var array
+	 * @SerializationPropertyAttribute()
 	 */
 	protected array $value;
 
