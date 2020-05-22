baf68aa92677c4f33944c98d8fe2924b08d5f852
Christian Ramelow <info@codekandis.net>
2020-05-22 17:28:30 +0200

    Adds the excpetion `IoException`.

---
+++ src/Io/IoException.php
@@ -0,0 +1,13 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Shorty\Io;
+
+use CodeKandis\Shorty\AccessErrorException;
+
+/**
+ * Represents the exception if an IO error occured.
+ * @package codekandis/shorty
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class IoException extends AccessErrorException
+{
+}
