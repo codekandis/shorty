8cd2cd464c7e3435165b10ca38b76ce3bbe26288
Christian Ramelow <info@codekandis.net>
2020-05-22 17:38:12 +0200

    Adds the exception `FileSystemEntryNotDeletableException`.

---
+++ src/Io/FileSystemEntryIsNotDeletableException.php
@@ -0,0 +1,11 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Shorty\Io;
+
+/**
+ * Represents the exception if a file system entry is not deletable.
+ * @package codekandis/shorty
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class FileSystemEntryIsNotDeletableException extends IoException
+{
+}
