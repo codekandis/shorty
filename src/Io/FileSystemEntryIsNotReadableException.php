dc9c9863fd912ce589ab3eba5185d2167e92eace
Christian Ramelow <info@codekandis.net>
2020-05-22 17:37:09 +0200

    Adds the exception `FileSystemEntryNotReadableException`.

---
+++ src/Io/FileSystemEntryIsNotReadableException.php
@@ -0,0 +1,11 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Shorty\Io;
+
+/**
+ * Represents the exception if a file system entry is not readable.
+ * @package codekandis/shorty
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class FileSystemEntryIsNotReadableException extends IoException
+{
+}
