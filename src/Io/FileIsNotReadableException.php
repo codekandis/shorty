edf7e60027ba36d90ae89f93558c8eca776dc851
Christian Ramelow <info@codekandis.net>
2020-05-22 17:32:54 +0200

    Adds the exception `FileNotReadableException`.

---
+++ src/Io/FileIsNotReadableException.php
@@ -0,0 +1,11 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Shorty\Io;
+
+/**
+ * Represents the exception if a file is not readable.
+ * @package codekandis/shorty
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class FileIsNotReadableException extends FileSystemEntryIsNotReadableException
+{
+}
