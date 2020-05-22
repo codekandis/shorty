050cb3a970d8aba88ad7273b5cce8109774f1ef8
Christian Ramelow <info@codekandis.net>
2020-05-22 17:37:23 +0200

    Adds the exception `FileSystemEntryNotWritableException`.

---
+++ src/Io/FileSystemEntryIsNotWritableException.php
@@ -0,0 +1,11 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Shorty\Io;
+
+/**
+ * Represents the exception if a file system entry is not writable.
+ * @package codekandis/shorty
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class FileSystemEntryIsNotWritableException extends IoException
+{
+}
