d1a0748051bcf26fbd455886f7a0cacfb7c8d7a1
Christian Ramelow <info@codekandis.net>
2020-05-28 01:33:23 +0200

    Adds the exception `UriIsInvalidException`.

---
+++ src/Io/Streams/UriIsInvalidException.php
@@ -0,0 +1,13 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Shorty\Io\Streams;
+
+use CodeKandis\Shorty\ValueIsInvalidException;
+
+/**
+ * Represents an exception if an URI is invalid.
+ * @package codekandis/shorty
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class UriIsInvalidException extends ValueIsInvalidException
+{
+}
