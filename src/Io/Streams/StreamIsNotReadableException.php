8d5724fccd531051e5eb1b2a2054c5fa444d6d76
Christian Ramelow <info@codekandis.net>
2020-07-30 15:10:43 +0200

    Adds the exception `StreamIsNotReadableException`.

---
+++ src/Io/Streams/StreamIsNotReadableException.php
@@ -0,0 +1,11 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Shorty\Io\Streams;
+
+/**
+ * Represents the exception if a stream is not readable.
+ * @package codekandis/shorty
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class StreamIsNotReadableException extends StreamException
+{
+}
