9e639b4b56e57e0970c994b2bc2f1793d293afb3
Christian Ramelow <info@codekandis.net>
2020-07-30 14:58:19 +0200

    Adds the exception `StreamException`.

---
+++ src/Io/Streams/StreamException.php
@@ -0,0 +1,13 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Shorty\Io\Streams;
+
+use CodeKandis\Shorty\Io\IoException;
+
+/**
+ * Represents the exception if an error on stream operation occurred.
+ * @package codekandis/shorty
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class StreamException extends IoException
+{
+}
