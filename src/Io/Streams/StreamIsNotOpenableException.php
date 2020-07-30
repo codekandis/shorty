5da63268d1b604d70ecafc619d9c818d18c899ac
Christian Ramelow <info@codekandis.net>
2020-07-30 15:08:28 +0200

    Adds the exception `StreamIsNotOpenableException`.

---
+++ src/Io/Streams/StreamIsNotOpenableException.php
@@ -0,0 +1,11 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Shorty\Io\Streams;
+
+/**
+ * Represents the exception if a stream is not openable.
+ * @package codekandis/shorty
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class StreamIsNotOpenableException extends StreamException
+{
+}
