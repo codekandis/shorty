19458ca271e1e959d3142f357337b61bf4f05747
Christian Ramelow <info@codekandis.net>
2020-07-30 15:07:20 +0200

    Adds the exception `StreamHasAlreadyBeenOpenedException`.

---
+++ src/Io/Streams/StreamHasAlreadyBeenOpenedException.php
@@ -0,0 +1,11 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Shorty\Io\Streams;
+
+/**
+ * Represents the exception if a stream has already been opened.
+ * @package codekandis/shorty
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class StreamHasAlreadyBeenOpenedException extends StreamException
+{
+}
