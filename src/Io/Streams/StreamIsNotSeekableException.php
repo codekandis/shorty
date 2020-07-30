93e546297b126fda66337b513f3d09b40cd6fef0
Christian Ramelow <info@codekandis.net>
2020-07-30 15:10:11 +0200

    Adds the exception `StreamIsNotSeekableException`.

---
+++ src/Io/Streams/StreamIsNotSeekableException.php
@@ -0,0 +1,11 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Shorty\Io\Streams;
+
+/**
+ * Represents the exception if a stream is not seekable.
+ * @package codekandis/shorty
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class StreamIsNotSeekableException extends StreamException
+{
+}
