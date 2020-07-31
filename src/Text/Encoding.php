bd0d453f241861826b9c4851e00d5241d4e959af
Christian Ramelow <info@codekandis.net>
2020-07-31 14:32:05 +0200

    Makes the class `Encoding` serializable.

--- src/Text/Encoding.php
+++ src/Text/Encoding.php
@@ -1,6 +1,7 @@
 <?php declare( strict_types = 1 );
 namespace CodeKandis\Shorty\Text;
 
+use CodeKandis\Shorty\Data\Serialization\SerializationContractAttribute;
 use CodeKandis\Shorty\Types\Wrapped\AbstractString;
 
 /**
@@ -8,6 +9,7 @@ use CodeKandis\Shorty\Types\Wrapped\AbstractString;
  * @see https://www.php.net/manual/en/mbstring.supported-encodings.php
  * @package codekandis/shorty
  * @author Christian Ramelow <info@codekandis.net>
+ * @SerializationContractAttribute( serializeSinglePropertyOnly = true )
  */
 class Encoding extends AbstractString implements EncodingInterface
 {
